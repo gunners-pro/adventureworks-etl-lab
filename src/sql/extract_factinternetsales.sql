3064167bffb0aa635274726868b87314cab8ee71
Fabricyo Barreto <bass.barreto@gmail.com>
2026-06-23 21:06:20 -0300

    Add SQL query to extract internet sales data

--- src/sql/extract_factinternetsales.sql
+++ src/sql/extract_factinternetsales.sql
@@ -1,3 +1,4 @@
+-- T-SQL / SQL Server
 SELECT ProductKey, OrderDateKey, DueDateKey, ShipDateKey, CustomerKey, PromotionKey, CurrencyKey, SalesOrderNumber, SalesOrderLineNumber,
     RevisionNumber, OrderQuantity, UnitPrice, ExtendedAmount, UnitPriceDiscountPct, DiscountAmount, ProductStandardCost, TotalProductCost,
     SalesAmount, TaxAmt, Freight, CarrierTrackingNumber, CustomerPONumber, OrderDate, DueDate, ShipDate
