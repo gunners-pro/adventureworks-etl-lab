608184670722ff42cac5eca087190b41f7a1223d
Fabricyo <bass.barreto@gmail.com>
2026-06-23 21:20:37 -0300

    Revert "Add SQL query to extract internet sales data"
    This reverts commit 3064167bffb0aa635274726868b87314cab8ee71.

--- src/sql/extract_factinternetsales.sql
+++ src/sql/extract_factinternetsales.sql
@@ -1,4 +1,3 @@
--- T-SQL / SQL Server
 SELECT ProductKey, OrderDateKey, DueDateKey, ShipDateKey, CustomerKey, PromotionKey, CurrencyKey, SalesOrderNumber, SalesOrderLineNumber,
     RevisionNumber, OrderQuantity, UnitPrice, ExtendedAmount, UnitPriceDiscountPct, DiscountAmount, ProductStandardCost, TotalProductCost,
     SalesAmount, TaxAmt, Freight, CarrierTrackingNumber, CustomerPONumber, OrderDate, DueDate, ShipDate
